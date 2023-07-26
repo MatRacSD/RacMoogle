d4e186703d308041872c497179b296e051cfb154
MatRacSD <racielsimondomenech@gmail>
2023-07-25 23:06:38 -0500

    actualización para práctica laboral

---
+++ script.sh
@@ -0,0 +1,50 @@
+
+
+if [[ "$1" == "run" ]]; then
+    if [[ "$OSTYPE" == "msys" ]] || [[ "$OSTYPE" == "cygwin" ]]; then
+      powershell.exe -Command "cd MoogleServer; dotnet watch run"
+    elif [[ "$OSTYPE" == "darwin"* ]]; then
+        open -a Terminal.app MoogleServer -n --args bash -c "dotnet watch run"
+    elif [[ "$OSTYPE" == "linux-gnu" ]]; then
+        gnome-terminal --working-directory="$PWD/MoogleServer" -x bash -c "dotnet watch run"
+    fi
+    sleep 5 
+    if [[ "$OSTYPE" == "msys" ]] || [[ "$OSTYPE" == "cygwin" ]]; then
+        start "http://localhost:5285"
+    elif [[ "$OSTYPE" == "darwin"* ]]; then
+        open "http://localhost:5285"
+    elif [[ "$OSTYPE" == "linux-gnu" ]]; then
+        xdg-open "http://localhost:5285"
+    fi
+
+elif [[ "$1" == "report" ]]; then
+    pdflatex informe.tex
+
+elif [[ "$1" == "slides" ]]; then
+    pdflatex presentacion.tex
+
+
+elif [[ "$1" == "show_slides" ]]; then
+    pdflatex presentacion.tex
+    if [[ "$OSTYPE" == "msys" ]] || [[ "$OSTYPE" == "cygwin" ]]; then
+        start "$(cygpath -w presentacion.pdf)"
+    elif [[ "$OSTYPE" == "darwin"* ]]; then
+        open presentacion.pdf
+    elif [[ "$OSTYPE" == "linux-gnu" ]]; then
+        xdg-open presentacion.pdf
+    fi
+elif [[ "$1" == "show_report" ]]; then
+    pdflatex informe.tex
+    if [[ "$OSTYPE" == "msys" ]] || [[ "$OSTYPE" == "cygwin" ]]; then
+        start "$(cygpath -w informe.pdf)"
+    elif [[ "$OSTYPE" == "darwin"* ]]; then
+        open informe.pdf
+    elif [[ "$OSTYPE" == "linux-gnu" ]]; then
+        xdg-open informe.pdf
+    fi
+
+elif [[ "$1" == "clean" ]]; then
+    find . -type f -regextype posix-extended -regex '.*\.(aux|bbl|fdb_latexmk|fls|pdf|gz|toc|log|nav|out|snm)' -delete
+    find . -name ".vscode" -type d -exec rm -rf {} +
+    find . -type d -name "bin" -o -name "obj" -exec rm -rf {} +
+fi
